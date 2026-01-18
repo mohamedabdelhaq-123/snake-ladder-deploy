4643124de4f4be56579b47b92b8f8512dd29d0c3
ZeyadHMostafa <ZeyadHMostafa@gmail.com>
2026-01-18 14:23:59 +0200

    changed linting for comfort

--- eslint.config.js
+++ eslint.config.js
@@ -1,41 +1,45 @@
 // eslint.config.js
+// eslint-disable-next-line no-undef
 module.exports = [
-  {
-    files: ['**/*.js'],
-    ignores: ['node_modules/**', 'dist/**', 'build/**'],
+	{
+		files: ["**/*.js"],
+		ignores: ["node_modules/**", "dist/**", "build/**"],
 
-    languageOptions: {
-      ecmaVersion: 2022,
-      sourceType: 'module',
-      globals: {
-        window: 'readonly',
-        document: 'readonly',
-        console: 'readonly',
-        fetch: 'readonly',
-      },
-    },
+		languageOptions: {
+			ecmaVersion: 2022,
+			sourceType: "module",
+			globals: {
+				window: "readonly",
+				document: "readonly",
+				console: "readonly",
+				fetch: "readonly",
+			},
+		},
 
-    rules: {
-      // Prevent bugs
-      'no-debugger': 'error',
-      'no-duplicate-case': 'error',
-      'no-extra-semi': 'error',
-      'no-unreachable': 'error',
-      'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
-      'no-undef': 'error',
+		rules: {
+			// Prevent bugs
+			"no-debugger": "error",
+			"no-duplicate-case": "error",
+			"no-extra-semi": "error",
+			"no-unreachable": "error",
+			"no-unused-vars": ["error", {
+				varsIgnorePattern: "^[A-Z][a-z]",
+				argsIgnorePattern: "^_"
+			}],
+			"no-undef": "error",
 
-      // Code quality
-      'eqeqeq': ['error', 'always'],
-      'no-eval': 'error',
-      'no-var': 'error',
-      'curly': ['error', 'all'],
+			// Code quality
+			"eqeqeq": ["error", "always"],
+			"no-eval": "error",
+			"no-var": "error",
+			"curly": ["error", "all"],
 
-      // Formatting
-      'indent': ['error', 2],
-      'quotes': ['error', 'single', { avoidEscape: true }],
-      'semi': ['error', 'always'],
-      'no-trailing-spaces': 'error',
-      'keyword-spacing': 'error',
-    },
-  },
-]
+			// Formatting
+			"indent": ["error", "tab"],
+			"quotes": ["error", "double", { avoidEscape: true }],
+			"semi": ["error", "always"],
+			"no-trailing-spaces": "error",
+			"keyword-spacing": "error",
+		},
+	},
+];
