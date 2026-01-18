c8984a9fe845bc4877bf3d50b962eca8af296977
Andrew Emad <andrewemad@Andrews-MacBook-Pro.local>
2026-01-18 14:20:41 +0200

    rename: leaderboard files are renamed for better readablity

---
+++ js/pages/leaderboard.js
@@ -0,0 +1,3 @@
+let homeBtn = document.getElementById("home-button");
+let instructionBtn = document.getElementById("instruction-button");
+let playAgainBtn = document.getElementById("play-again-button");
